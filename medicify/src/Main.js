1680e3aa79ed22d334214d4bfdd4975f05fa8456
Sree Ram <sree098245@gmail.com>
2024-01-29 19:42:22 +0530

    react router domc created

--- medicify/src/Main.js
+++ medicify/src/Main.js
@@ -2,14 +2,21 @@ import './App.css';
 import Navbar from './components/html_part/Navbar';
 import Cart from './components/html_part/Cart';
 import Login from './components/html_part/Login';
+import { Routes,Route } from "react-router-dom";
 
 
 function App() {
   return (
     <>
     <Navbar/>
-    <Login/>
-    <Cart/>
+
+    <Routes>
+      <Route path="/*" element={<h1>No route found here.</h1>} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/cart" element={<Cart/>} />
+    </Routes>
+    
+    
     </>
   );
 }
